5e71aa2a834383c78f64348077aafccd6e0ddcc1
Vijayalakshmi V <vijayalakshmi9418@gmail.com>
2026-07-09 10:06:57 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 10:06:28
+class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> count = new HashMap<>();
+        
+        for (char c : t.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+        
+        for (char c : s.toCharArray()) {
+            count.put(c, count.get(c) - 1);
+            if (count.get(c) == 0) {
+                count.remove(c);
+            }
+        }
+        
+        return (char) count.keySet().toArray()[0];        
+    }
+}
